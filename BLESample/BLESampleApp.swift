5eeab3556fc843b2ea1db24ec69dfb9f66b46721
Greg Alton <gregaltonemail@gmail.com>
2024-05-09 13:41:54 -0500

    Initial Commit

---
+++ BLESample/BLESampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLESampleApp.swift
+//  BLESample
+//
+//  Created by Greg Alton on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct BLESampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
